4fbe05a351b1f84f5f250bc9174811e61f838dd0
Adrix1 <adrixmalagon@gmail.com>
2017-04-21 13:10:23 -0500

    Se agregaron links en las imagenes de la sección de Herramientas y Modal falta la información del mismo, se cambiaron las imágnes del Slider principal, se modifico el formato del boton de los modals

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-$(document).ready(function() {
-	var carousel = $("#carousel").featureCarousel({
-	});
-	$("#but_prev").click(function () {
-		carousel.prev();
-	});
-	$("#but_pause").click(function () {
-		carousel.pause();
-	});
-	$("#but_start").click(function () {
-		carousel.start();
-	});
-	$("#but_next").click(function () {
-		carousel.next();
-	});
-});
-
 $(document).ready(function(){
     $("p").click(function(){
         $(this).hide();
